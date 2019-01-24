de7fbcf2e68220b982d8390bf0bca4e1b3d91595
Manzoor Wani <manzoorwani.jk@gmail.com>
2019-01-24 17:43:02 +0530

    Add optional argument to the helper function

--- html2text.php
+++ html2text.php
@@ -23,8 +23,8 @@
 require_once(__DIR__ . "/src/Html2Text.php");
 require_once(__DIR__ . "/src/Html2TextException.php");
 
-function convert_html_to_text($html) {
-	return Html2Text\Html2Text::convert($html);
+function convert_html_to_text($html, $ignore_error = false) {
+	return Html2Text\Html2Text::convert($html, $ignore_error);
 }
 
 function fix_newlines($text) {
