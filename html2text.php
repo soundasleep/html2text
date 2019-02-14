fec9299557a439ba3e670cb28ffc96e7373794f4
Guillaume RODRIGUEZ <1820051+guillaume-ro-fr@users.noreply.github.com>
2019-02-14 15:27:34 +0100

    Fix namespace in composer

--- html2text.php
+++ html2text.php
@@ -8,9 +8,9 @@ require_once(__DIR__ . "/src/Html2Text.php");
 require_once(__DIR__ . "/src/Html2TextException.php");
 
 function convert_html_to_text($html, $ignore_error = false) {
-	return Html2Text\Html2Text::convert($html, $ignore_error);
+	return Soundasleep\Html2Text::convert($html, $ignore_error);
 }
 
 function fix_newlines($text) {
-	return Html2Text\Html2Text::fixNewlines($text);
+	return Soundasleep\Html2Text::fixNewlines($text);
 }
